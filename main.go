feca0459a6dd48ff3309e87f5f1a71d42b5f182c
oYuYo <year0120@gmail.com>
2024-04-14 17:15:15 +0900

    up

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/base64"
 	"fmt"
 	"image"
 	"image/color/palette"
+	"image/draw"
 	"image/gif"
-	"image/jpeg"
 	"image/png"
 	"strconv"
 	"strings"
@@ -36,10 +35,10 @@ type PNGImg struct {
 func Convert(this js.Value, args []js.Value) interface{} {
 	offset := 2
 	var tmp string = args[0].String()
-	_, err := strconv.Atoi(tmp)
+	delay, err := strconv.Atoi(tmp)
 	if err != nil {
 		fmt.Println(err)
-		printAlert("秒数の取得に失敗しました")
+		printAlert("時間の取得に失敗しました")
 		return nil
 	}
 
@@ -50,6 +49,7 @@ func Convert(this js.Value, args []js.Value) interface{} {
 		return nil
 	}
 
+	var delays []int
 	var JPGImgs []JPGImg
 	var images []*image.Paletted
 
@@ -65,20 +65,15 @@ func Convert(this js.Value, args []js.Value) interface{} {
 			printAlert("PNGデータのデコードに失敗しました")
 			return nil
 		}
-		palettedImg := image.NewPaletted(img.Bounds(), palette.WebSafe)
-		//palettedImg := image.NewPaletted(img.Bounds(), nil)
-		//draw.Draw(palettedImg, palettedImg.Bounds(), img, image.Point{}, draw.Over)
+		palettedImg := image.NewPaletted(img.Bounds(), palette.Plan9)
+		draw.Draw(palettedImg, palettedImg.Bounds(), img, image.Point{}, draw.Over)
 		images = append(images, palettedImg)
 
-		var b bytes.Buffer
-		if err := jpeg.Encode(bufio.NewWriter(&b), img, &jpeg.Options{Quality: 90}); err != nil {
-			printAlert("JPGデータへのエンコードに失敗しました")
-			return nil
-		}
+		delays = append(delays, delay)
 	}
 
 	var gifData bytes.Buffer
-	gif.EncodeAll(&gifData, &gif.GIF{Image: images, Delay: []int{100}})
+	gif.EncodeAll(&gifData, &gif.GIF{Image: images, Delay: delays})
 
 	_, err = createGIF(&JPGImgs)
 	if err != nil {
@@ -89,54 +84,6 @@ func Convert(this js.Value, args []js.Value) interface{} {
 	attachData(gifData.Bytes(), "output", ".gif")
 	return nil
 }
-
-/*
-func fillTransparentWhite(img image.Image) image.Image {
-	bounds := img.Bounds()
-	newImg := image.NewRGBA(bounds)
-
-	white := color.RGBA{255, 255, 255, 255}
-
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			_, _, _, alpha := img.At(x, y).RGBA()
-			if alpha == 0 {
-				newImg.Set(x, y, white)
-			} else {
-				newImg.Set(x, y, img.At(x, y))
-			}
-		}
-	}
-	return newImg
-}
-
-func resizeImage(resizeType ResizeType, specifiedfileSize int, img image.Image) image.Image {
-	origBounds := img.Bounds()
-	origWidth := origBounds.Dx()
-	origHeight := origBounds.Dy()
-	var ratio float64
-
-	if resizeType == Horizontal {
-		ratio = float64(specifiedfileSize) / float64(origWidth)
-	} else {
-		ratio = float64(specifiedfileSize) / float64(origHeight)
-	}
-	newWidth := int(float64(origWidth) * ratio)
-	newHeight := int(float64(origHeight) * ratio)
-
-	resizeImg := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
-
-	for y := 0; y < newHeight; y++ {
-		for x := 0; x < newWidth; x++ {
-			origX := int(float64(x) / ratio)
-			origY := int(float64(y) / ratio)
-			resizeImg.Set(x, y, img.At(origX, origY))
-		}
-	}
-
-	return resizeImg
-}
-*/
 
 func createGIF(data *[]JPGImg) ([]byte, error) {
 	var gifData bytes.Buffer
